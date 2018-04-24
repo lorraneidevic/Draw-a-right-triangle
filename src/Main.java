6a11f5ef5a1387292c8ab74eda1e29386dfa94b7
Lorrane Idevic Gonçalves <lorraneidevic@MacBook-Pro-de-Lorrane.local>
2018-04-24 17:24:54 -0300

    loop printing the right triangle

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main (String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please type the number (integer) of lines of the triangle:");
+        int numberOfLines = scanner.nextInt();
+
+        for(int i = 0; i < numberOfLines; i++) {
+            for(int j = 0; j <= i; j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
     }
 }
